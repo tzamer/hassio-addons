11965a04b1103d848aca55f7239052a4d67e9fe5
gbrandon@vmware.com <gbrandon@vmware.com>
2017-07-31 11:31:36 -0400

    Cleanup

--- node-red/run.sh
+++
@@ -1,9 +0,0 @@
-CONFIG_PATH=/data/options.json
-
-cd /data
-# npm install node-red-node-smooth
-
-jq --raw-output ".installnodes[]" $CONFIG_PATH | while read p; do echo "Ensuring $p is installed…"; npm install -g $p; done
-
-cd /usr/src/node-red
-npm start --userDir /data
